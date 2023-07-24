444c0d4730520bd555dd9da731c00f0a8a2ad7ed
goodnesstomiwa <arinolagoodness@gmail.com>
2023-07-24 16:30:09 -0700

    File fixed

--- get_precision.c
+++ get_precision.c
@@ -15,21 +15,25 @@ int precision = -1;
 if (format[curr_i] != '.')
 return (precision);
 precision = 0;
-for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
+curr_i++;
+if (format[curr_i] == '*')
+{
+curr_i++;
+precision = va_arg(list, int);
+}
+else
+{
+while (format[curr_i] != '\0')
 {
 if (is_digit(format[curr_i]))
 {
 precision *= 10;
-precision += format[curr_i] - '0';
-}
-else if (format[curr_i] == '*')
-{
+precison += format[curr_i] - '\0';
 curr_i++;
-precision = va_arg(list, int);
-break;
 }
 else
 break;
+}
 }
 *i = curr_i - 1;
 return (precision);
