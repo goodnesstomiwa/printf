e2ba733ac875091e0eb861d1b576dcbbe968d0de
goodnesstomiwa <arinolagoodness@gmail.com>
2023-07-24 17:30:34 -0700

    File fixed

--- get_precision.c
+++ get_precision.c
@@ -1,52 +1,37 @@
 #include "main.h"
 
 /**
-* is_digit - Checks if a character is a digit (0-9).
-* @c: The character to check.
-*
-* Return: 1 if the character is a digit, 0 otherwise.
-*/
-static int is_digit(char c)
-{
-return (c >= '0' && c <= '9');
-}
-
-/**
-* get_precision - Calculates the precision for printing.
-* @format: Formatted string in which to print the arguments.
-* @i: Pointer to the index in the format string.
-* @list: List of arguments.
+* get_precision - Calculates the precision for printing
+* @format: Formatted string in which to print the arguments
+* @i: List of arguments to be printed.
+* @list: list of arguments.
 *
 * Return: Precision.
 */
+
 int get_precision(const char *format, int *i, va_list list)
 {
 int curr_i = *i + 1;
 int precision = -1;
-
-if (format[curr_i] == '.')
-{
+if (format[curr_i] != '.')
+return (precision);
 precision = 0;
-curr_i++;
-
-if (format[curr_i] == '*')
+for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
+{
+if (is_digit(format[curr_i]))
+{
+precision *= 10;
+precision += format[curr_i] - '0';
+}
+else if (format[curr_i] == '*')
 {
 curr_i++;
-int dynamic_precision = va_arg(list, int);
-if (dynamic_precision >= 0)
-precision = dynamic_precision;
+precision = va_arg(list, int);
+break;
 }
 else
-{
-while (is_digit(format[curr_i]))
-{
-precision = precision * 10 + (format[curr_i] - '0');
-curr_i++;
+break;
 }
-}
-}
-
 *i = curr_i - 1;
 return (precision);
 }
-
